8dd76e4311afe28f25a367e71dc5db10131c4439
xieting <991283109@qq.com>
2018-11-22 10:46:49 +0800

    监测用户登录状态

--- src/containers/main/main.jsx
+++ src/containers/main/main.jsx
@@ -1,16 +1,37 @@
 import React, {Component} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
+import Cookies from 'js-cookie';
 
 import LaobanInfo from '../laoban-info/laoban-info';
 import DashenInfo from '../dashen-info/dashen-info';
+import {getRedirectTo} from "../../utils";
 
 class Main extends Component {
+    componentDidMount() {
+        const userid = Cookies.get('userid');
+        const {_id} = this.props.user;
+        if (userid && !_id) {
+            console.log(1);
+        }
+    }
+
     render() {
-        //检查用户是否登录
+        //读取cookie中的userid，检查用户是否登录
+        const userid = Cookies.get('userid');
+        if (!userid) {
+            return <Redirect to='/login'/>
+        }
+
         const {user} = this.props;
         if (!user._id) {
-            return <Redirect to='/login'/>
+            return null;
+        } else {
+            let path = this.props.location.pathname;
+            if (path === '/') {
+                path = getRedirectTo(user.type, user.header);
+                return <Redirect to={path}/>
+            }
         }
 
         return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, HashRouter, Route, Switch} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
 import Regist from './containers/regist/regist';
