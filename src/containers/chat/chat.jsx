e4b4b0eb8c4342a360f78ffae2d9381cbdc56de3
xieting <991283109@qq.com>
2018-11-26 11:27:09 +0800

    添加表情

--- src/containers/chat/chat.jsx
+++ src/containers/chat/chat.jsx
@@ -4,6 +4,8 @@ import {
     NavBar,
     List,
     InputItem,
+    Grid,
+    Icon,
 } from 'antd-mobile';
 
 import {sendMsg} from '../../redux/action';
@@ -12,8 +14,22 @@ const Item = List.Item;
 
 class Chat extends Component {
     state = {
-        content: ''
+        content: '',
+        showEmoji: false
     };
+
+    componentWillMount() {  //在第一次render之前回调
+        const emojis = ['😁', '😂', '😃', '😉', '😊', '😁', '😂', '😃', '😉', '😊', '😁', '😂', '😃', '😉', '😊', '😁', '😂', '😃', '😁', '😂', '😃', '😉', '😊', '😁', '😂', '😃', '😁', '😂', '😃', '😉', '😊', '😁', '😂', '😃', '😁', '😂', '😃', '😉', '😊', '😁', '😂', '😃',];
+        this.emojis = emojis.map(emoji => ({text: emoji}));
+    }
+
+    componentDidMount() {  //初始时滑到底部
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+
+    componentDidUpdate() {  //更新对话时滑到底部
+        window.scrollTo(0, document.body.scrollHeight);
+    }
 
     sendMsg = () => {
         const from = this.props.user._id;
@@ -22,7 +38,17 @@ class Chat extends Component {
         if (content) {
             this.props.sendMsg({from, to, content});
         }
-        this.setState({content: ''});
+        this.setState({content: '', isShow: false});
+    };
+
+    toggleShow = () => {  //交替显示表情
+        const isShow = !this.state.isShow;
+        this.setState({isShow});
+        if (isShow) {
+            setTimeout(() => {
+                window.dispatchEvent(new Event('resize'))
+            }, 0)
+        }
     };
 
     render() {
@@ -41,8 +67,15 @@ class Chat extends Component {
         // console.log(targetId, users, chatMsgs, users[targetId]);
         return (
             <div id='chat-page'>
-                <NavBar>{users[targetId].username}</NavBar>
-                <List>
+                <NavBar className='stick-top'
+                        icon={<Icon type='left'/>}
+                        onLeftClick={() => {
+                            this.props.history.goBack()
+                        }}
+                >
+                    {users[targetId].username}
+                </NavBar>
+                <List style={{marginTop: 50, marginBottom: 50}}>
                     {
                         msgs.map((msg, index) => {
                             if (myid === msg.to) {
@@ -61,11 +94,30 @@ class Chat extends Component {
                         placeholder='请输入'
                         value={this.state.content}
                         onChange={val => this.setState({content: val})}
+                        onFocus={() => this.setState({isShow: false})}
                         extra={
-                            <span onClick={this.sendMsg}>发送</span>
+                            <span>
+                                <span
+                                    onClick={this.toggleShow}
+                                    style={{marginRight: 5}}>😁</span>
+                                <span onClick={this.sendMsg}>发送</span>
+                            </span>
                         }
                     />
                 </div>
+                {
+                    this.state.isShow ? (<Grid
+                            data={this.emojis}
+                            columnNum={8}
+                            carouselMaxRow={4}
+                            isCarousel={true}
+                            onClick={(item) => {
+                                this.setState({content: this.state.content + item.text})
+                            }}
+                            style={{position: 'fixed', bottom: 0}}
+                        />)
+                        : null
+                }
             </div>
         );
     }
